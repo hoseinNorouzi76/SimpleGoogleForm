9f69540a5175533e1b3944ee3f263c69fe01ab15
hoseinNorouzi76 <44918543+hoseinNorouzi76@users.noreply.github.com>
2019-02-18 16:09:29 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const express = require('express')
 const router = require('./router')
 const database = require('./database/database')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 database.connect('form_db')
